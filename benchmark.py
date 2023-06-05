84362f0db0d863ea02ee0709668621fa35196826
erietz <ewrietz@gmail.com>
2023-06-05 08:04:07 -0500

    add a plot

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,7 @@
 import subprocess
 from pathlib import Path
-# import json
-# from matplotlib import pyplot as plt
+import json
+from matplotlib import pyplot as plt
 
 low = 0
 high = 100000
@@ -14,24 +14,49 @@ def run_benchmark():
 
     n_partitions = 1
     while n_partitions < high:
-        n_partitions *= 2
-
-        inner_cmd = [
-            './prime',
-            '--lo',
-            str(low),
-            '--hi',
-            str(high),
-            '--nPartitions',
-            str(n_partitions)
-        ]
+        inner_cmd = f'./prime --lo {low} --hi {high} --nPartitions {n_partitions}'
 
         cmd = [
             'hyperfine',
             '--export-json',
             f'./benchmark/lo_{low}_hi_{high}_nPartitions_{n_partitions}.json',
-            ' '.join(inner_cmd)
+            inner_cmd
         ]
 
         print("running", cmd)
         subprocess.run(cmd)
+        n_partitions *= 2
+
+
+def load_data():
+    data = []
+    for file in dest_dir.glob("*.json"):
+        with open(file) as f:
+            data.append(json.load(f)["results"][0])
+    return data
+
+
+def plot():
+    data = load_data()
+
+    commands = [d["command"] for d in data]
+    goroutines = [int(c.split(" ")[-1]) for c in commands]
+    means = [d["mean"] for d in data]
+    stddev = [d["stddev"] for d in data]
+    # print(sorted(zip(*[goroutines, commands, means, stddev])))
+
+    data = sorted(zip(*[goroutines, commands, means, stddev]))
+    goroutines = [i[0] for i in data]
+    commands = [i[1] for i in data]
+    means = [i[2] for i in data]
+    stddev = [i[3] for i in data]
+
+    plt.bar(goroutines, means, yerr=stddev, width=200)
+    plt.xlabel("goroutines")
+    plt.ylabel("time / ms")
+    plt.savefig('./benchmark.png')
+    plt.show()
+
+
+# run_benchmark()
+plot()
